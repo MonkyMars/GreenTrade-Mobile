a96bb8d25b177e62615084ccbd4b3c87aedaf027
Levi Noppers <levinoppers69@gmail.com>
2025-04-08 17:19:44 +0200

    fix: update BASE_URL in axiosConfig to use production endpoint; add eas.json for EAS configuration

--- lib/backend/api/axiosConfig.ts
+++ lib/backend/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import axios from 'axios'
 
-const BASE_URL = 'http://192.168.178.10:8080'
+const BASE_URL = 'https://greentradeeu.up.railway.app'
 
 axios.interceptors.request.use(config => {
   const token = AsyncStorage.getItem('accessToken')
